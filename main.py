bb1402379195acc24dac42fe22fe283f94dd1712
EmmanuelMadehin <tobimadehin@gmail.com>
2022-12-13 16:26:03 +0100

    Made few changes

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 userName = "Black"
 userPlanet = ['Deutron', 'Axton', 'Capricon', 'Galacticon']
+
 name = input("Enter your name: ")
-if name < "9":
-  print(name)
+
+if len(name) >= 3 & len(name) <= 10:
+  print(name + " is a valid username")
+
+else: print("Username is invalid! Try a username between 3 and 10")
