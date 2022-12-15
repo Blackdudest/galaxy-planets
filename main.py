6997666145756b230d8eac13da4324cafec4e597
Blackdudest <adeoyesamson24@gmail.com>
2022-12-15 11:54:25 +0100

    Made some correction

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
+import random
+
 userName = "Black"
 userPlanet = ['Deutron', 'Axton', 'Capricon', 'Galacticon']
 
 name = input("Enter your name: ")
+nameLength = len(name)
+print("nameLength is " + str(nameLength))
 
-if len(name) >= 3 & len(name) <= 10:
+if 2 < nameLength < 11:
   print(name + " is a valid username")
+  planet = input("Choose your planet: ")
+  userPlanet = ['Deutron', 'Axton', 'Capricon', 'Galacticon']
 
-else: print("Username is invalid! Try a username between 3 and 10")
+  if planet in userPlanet:
+   print(planet + " is valid")
+  
+  
+
+   numberInput = int(input("Guess a number between 1000 to 2500: "))
+   randomNumber = random.randint(1000, 2500)
+   print(str(randomNumber) + " is valid")
+
+   if numberInput <= randomNumber:
+     print("You lose")
+
+   else: 
+     print("You win")
+  else: 
+   print(planet + " is invalid! Choose between Deutron, Axton, Capricon, Galacticon")
+
+
+else: 
+  print("Username is invalid! Try a username between 3 and 10")
+
+
+
+
